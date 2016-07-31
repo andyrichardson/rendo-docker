2e399e4cb5b5beb801fed085b15e98a272b5223b
Andy Richardson <contact@andythedeveloper.com>
2016-07-31 22:19:53 +0100

    naming changes for api container

--- dev/init_api.sh
+++ dev/init_api.sh
@@ -3,10 +3,10 @@
 docker build -t rendo-api:dev -f dev/api/Dockerfile .
 
 # Kill any previous containers
-docker rm api-dev 2> /dev/null
+docker rm rendo-api-dev 2> /dev/null
 
 # Run container
 docker run -t -i -p 80:80 \
 -v $PWD/../rendo-api:/var/www/rendo-api \
---name=rendo-api \
+--name=rendo-api-dev \
 rendo-api:dev
