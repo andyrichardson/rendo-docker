423242d864e309ae33fb9aa0a0f44155509b0c8a
Andy Richardson <contact@andythedeveloper.com>
2016-07-31 21:52:17 +0100

    Naming for easier admin & network identification

--- dev/init_api_container.sh
+++ dev/init_api_container.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-docker build -t api_container:dev -f dev/api/Dockerfile .
-docker run -t -i -p 80:80 -v $PWD/../rendo-api:/var/www/rendo-api api_container:dev
+# Build container
+docker build -t rendo-api:dev -f dev/api/Dockerfile .
+
+# Kill any previous containers
+docker rm api-dev 2> /dev/null
+
+# Run container
+docker run -t -i -p 80:80 \
+-v $PWD/../rendo-api:/var/www/rendo-api \
+--name=rendo-api \
+rendo-api:dev
--- dev/init_neo4j.sh
+++ dev/init_neo4j.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# Build container
 docker build -t neo4j:dev -f dev/neo4j/Dockerfile .
-docker run -p 7474:7474 -v $PWD/../neo4j/data:/data neo4j:dev
+
+# Kill any previous containers
+docker rm noe4j-dev 2> /dev/null
+
+# Run container
+docker run -p 7474:7474 \
+-v $PWD/../neo4j/data:/data \
+--name=neo4j-dev \
+neo4j:dev
